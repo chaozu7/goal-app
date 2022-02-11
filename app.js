0fcd6ad58d357f881a2dc11bde790847bfb73a60
Kamila <k.nawrocka@lemonova.pl>
2022-02-11 16:48:02 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+Vue.createApp({
+    data(){
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enteredValue);
+            this.enteredValue ='';
+        }
+    }
+}).mount('#app');
+
+
+
+
+/* 
+Imperative approach - only JS
+
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEL = document.querySelector('ul');
+
+function addGoal(){
+    const enteredValue = inputEl.value;
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = enteredValue;
+    listEL.appendChild(listItemEl);
+    inputEl.value = '';
+};
+
+buttonEl.addEventListener('click', addGoal); */
+
